b24a1c88897290542aaffb486b0a05b7558e7886
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-04-06 12:59:31 -0400

    Brincando com o playground 2

---
+++ eggplant-brownie/MyPlayground.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+var name = "Romildo"
+name = "Romildo Paiter" ; println(name)
+
+let PROJECT = "Eggplant Brownie"
+println(PROJECT) ;
+
+
+var teste /* tesrt */  = "teste"
+
+/*
+resre
+*/
+
+
