a43b8ad1fcca15da43b560bc00f125f19741ccbd
Romildo Jozue Paiter <romildopaiter@gmail.com>
2015-04-06 13:14:36 -0400

    Efetuando testes no playground

--- eggplant-brownie/MyPlayground.playground/section-1.swift
+++ eggplant-brownie/MyPlayground.playground/section-1.swift
@@ -1,16 +1,22 @@
 // Playground - noun: a place where people can play
 
-var name = "Romildo"
-name = "Romildo Paiter" ; println(name)
+let name = "Dani's paradise"
+let happiness = 5 // Int
+// let name = "eggplant brownie"
+let calories = 50.5 // Double
+let eggplantBrownieIsVeggie = true
 
-let PROJECT = "Eggplant Brownie"
-println(PROJECT) ;
+func helloCalories(){
+    println("heloo calories")
+}
 
+helloCalories()
+println("Teste de duplicidade de metodo")
+helloCalories()
 
-var teste /* tesrt */  = "teste"
+func add(name: String, calories: Double){
+    println("adding: \(name) - \(calories)")
+}
 
-/*
-resre
-*/
-
-
+add("X Bagunça", 100.0)
+add("X Bagunça de Egg", 130.5)
